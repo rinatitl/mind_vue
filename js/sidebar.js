bcf50b6fc78b50569adc100db0654227d7f633f8
Rinat Farkhutdinov <Rinat.it2016@mail.ru>
2019-08-16 10:43:39 +0300

    fixed pathes to icons

--- js/sidebar.js
+++ js/sidebar.js
@@ -7,14 +7,14 @@ const sidebar = Vue.component('sidebar', {
                     </div>
                     <div id="page_links">
                         <ul>
-                            <li><a href="/" class="profile_link"></a></li>
-                            <li><a href="/voting" class="voting_link"></a></li>
-                            <li><a href="/events" class="events_link"> </a></li>
-                            <li><a href="/balance.html" class="balance_link"></a></li>
-                            <li><a href="/timetable" class="timetable_link"></a></li>
-                            <li><a href="/marks" class="marks_link"></a></li>
-                            <li><a href="/ask" class="ask_link"></a></li>
-                            <li><a href="/docs" class="docs_link"></a></li>
+                            <li><a href="index.html" class="profile_link"></a></li>
+                            <li><a href="voting.html" class="voting_link"></a></li>
+                            <li><a href="events.html" class="events_link"> </a></li>
+                            <li><a href="balance.html" class="balance_link"></a></li>
+                            <li><a href="timetable.html" class="timetable_link"></a></li>
+                            <li><a href="marks.html" class="marks_link"></a></li>
+                            <li><a href="ask.html" class="ask_link"></a></li>
+                            <li><a href="docs.html" class="docs_link"></a></li>
                         </ul>
                     </div>
                     <div class="sidebar_mask" :class="{ visible : maskVisible }" @click='sidebarClose'></div>
